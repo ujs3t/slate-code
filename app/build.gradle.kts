88d1e922f70125c12fbb8223d813c36767fe6a87
ujs3t <3merxy1991@gmail.com>
2026-09-08 19:50:26 +0300

    build: remove Firebase Google Services configuration
    Remove Google Services plugin and redundant placeholder file generation to clean up the build process and avoid dependency on google-services.json.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,9 @@
-import com.google.gms.googleservices.GoogleServicesPlugin.MissingGoogleServicesStrategy
-
 plugins {
   alias(libs.plugins.android.application)
   alias(libs.plugins.kotlin.compose)
   alias(libs.plugins.google.devtools.ksp)
   alias(libs.plugins.roborazzi)
   alias(libs.plugins.secrets)
-  alias(libs.plugins.google.services)
 }
 
 android {
@@ -70,8 +67,6 @@ secrets {
   defaultPropertiesFileName = ".env.example"
   ignoreList.add("FIREBASE_APPCHECK_DEBUG_TOKEN")
 }
-
-googleServices { missingGoogleServicesStrategy = MissingGoogleServicesStrategy.WARN }
 
 // Some unused dependencies are commented out below instead of being removed.
 // This makes it easy to add them back in the future if needed.
